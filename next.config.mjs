42175edf71ca54e4408e66d8cf99ca6cac1da2a4
Het Ashwinbhai Patel <patel.hetas@northeastern.edu>
2024-02-03 19:27:08 -0500

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
+  assetPrefix: "/portfolio",
 };
 
 export default nextConfig;
