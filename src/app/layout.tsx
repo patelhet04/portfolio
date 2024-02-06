fee29fce68de1a51d117f506b151eafc137673fd
Het Ashwinbhai Patel <patel.hetas@northeastern.edu>
2024-02-06 14:11:00 -0500

    added url icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/Memoji.png" type="image/icon type" />
+      </head>
       <body>
         <Loader />
         <div className="flex" data-theme="sunset">
